78760b3de44dfe444059670d2d1f2580fa2e6d50
Fedor Pozdnyakov <fedirit@yandex.ru>
2025-02-09 13:02:34 +0300

    DJ-106 DZ1 Change views.py and urls.py

--- first_project/app/views.py
+++ first_project/app/views.py
@@ -1,3 +1,42 @@
+# from django.http import HttpResponse
+# from django.shortcuts import render, reverse
+#
+#
+# def home_view(request):
+#     template_name = 'app/home.html'
+#     # впишите правильные адреса страниц, используя
+#     # функцию `reverse`
+#     pages = {
+#         'Главная страница': reverse('home'),
+#         'Показать текущее время': '',
+#         'Показать содержимое рабочей директории': ''
+#     }
+#
+#     # context и параметры render менять не нужно
+#     # подбробнее о них мы поговорим на следующих лекциях
+#     context = {
+#         'pages': pages
+#     }
+#     return render(request, template_name, context)
+#
+#
+# def time_view(request):
+#     # обратите внимание – здесь HTML шаблона нет,
+#     # возвращается просто текст
+#     current_time = None
+#     msg = f'Текущее время: {current_time}'
+#     return HttpResponse(msg)
+#
+#
+# def workdir_view(request):
+#     # по аналогии с `time_view`, напишите код,
+#     # который возвращает список файлов в рабочей
+#     # директории
+#     raise NotImplemented
+
+import datetime
+import os
+
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
@@ -8,12 +47,9 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse("time"),
+        'Показать содержимое рабочей директории': reverse("workdir")
     }
-    
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
     context = {
         'pages': pages
     }
@@ -21,15 +57,21 @@ def home_view(request):
 
 
 def time_view(request):
-    # обратите внимание – здесь HTML шаблона нет, 
+    # обратите внимание – здесь HTML шаблона нет,
     # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
-    return HttpResponse(msg)
+    current_time = datetime.datetime.now().time()
+    msg_time = f'Текущее время: {current_time}'
+    return HttpResponse(msg_time)
 
 
 def workdir_view(request):
     # по аналогии с `time_view`, напишите код,
-    # который возвращает список файлов в рабочей 
+    # который возвращает список файлов в рабочей
     # директории
-    raise NotImplemented
+    list_directory_elemtnts = []
+    path = '.'
+    result_file_list = os.listdir(path)
+    for file_element in result_file_list:
+        list_directory_elemtnts.append(f'{file_element}, ')
+    return HttpResponse(list_directory_elemtnts)
+    # raise NotImplemented
